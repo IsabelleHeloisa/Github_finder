9c526a5a61ffa855b9e61ce4b589c5c8dfe6be61
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-12-27 13:41:39 -0300

    feat: search by enter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,7 @@
 type SearchProps = {
   loadUser: (userName: string) => Promise<void>
 }
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 
 import { BsSearch } from 'react-icons/bs'
 
@@ -9,6 +9,12 @@ import classes from './Search.module.css'
 
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState('')
+
+  const handlekeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      loadUser(userName)
+    }
+  }
 
   return (
     <div className={classes.search}>
@@ -19,6 +25,7 @@ const Search = ({ loadUser }: SearchProps) => {
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={e => setUserName(e.target.value)}
+          onKeyDown={handlekeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
